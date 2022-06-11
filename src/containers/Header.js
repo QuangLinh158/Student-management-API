97f92d4a4316bc1dcbbcc0347c8b90d9e63a0279
QuangLinh158 <ASUS@QuangLinh.com>
2022-06-11 11:24:01 +0700

    test git new lap

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -15,40 +15,9 @@ const Header = () => {
 			type: ActionTypes.LOGOUT_USER,
 		});
 	};
-	return ( 
-    // <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-	//   <div className="container">
-
-	// 	<Link className="navbar-brand" to="/">Student Management</Link>
-
-	// 	<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-	// 	  <span className="navbar-toggler-icon"></span>
-	// 	</button>
-	// 	<div className="collapse navbar-collapse" id="navbarNav">
-	// 	  <ul className="navbar-nav me-auto">
-	// 		<li className="nav-item">
-	// 		  <Link className="nav-link" to="/major">Major</Link>
-	// 		</li>
-	// 		<li className="nav-item">
-	// 		  <Link className="nav-link" to="/instructor">Instructor</Link>
-	// 		</li>
-	// 		<li className="nav-item">
-	// 		  <Link className="nav-link" to="/student">Student</Link>
-	// 		</li>
-	// 	  </ul>
-	// 	  <ul className="navbar-nav">
-	// 			<li className="nav-item">
-	// 				<a className="nav-link" href="/#">welcome to ... </a>
-	// 			</li>
-	// 			<li className="nav-item">
-	// 				<a className="nav-link" href="/#"><i className="bi-box-arrow-right"></i></a>
-	// 			</li>
-	// 		</ul>
-	// 	</div>
-	//   </div>
-	// </nav>
-	<Navbar bg="primary" variant='dark' expand="md">
-  <Container>
+	return (
+	<Navbar bg="primary" sticky='top' variant='dark' expand="md">
+  	<Container>
     <Navbar.Brand as={Link} to="/">Student Management</Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
